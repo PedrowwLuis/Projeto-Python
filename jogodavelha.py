96a857ecce558b43c6adf161f4420493ed56123d
Migu3lzin <miguelluisjp2409@gmail.com>
2025-05-22 14:49:19 -0300

    criaçao das prints jogo da velha
    crie as prints

---
+++ jogodavelha.py
@@ -0,0 +1,5 @@
+print (bem vindo ao jogo da velha)
+print (você vai jogar contra o computador)
+print ( ganhar quem conseguir uma linha , coluna do diagonal do grid com o memso simbolo)
+
+escolha= input
