0b043ea9aadba6443dd4b0d48145cd632712a4da
Migu3lzin <miguelluisjp2409@gmail.com>
2025-05-26 18:26:10 -0300

    Update jogodavelha.py

--- jogodavelha.py
+++ jogodavelha.py
@@ -2,4 +2,19 @@ print ("Bem vindo ao Jogo da velha")
 print ("Você vai jogar contra o computador")
 print ("Ganha quem conseguir fazer uma linha, coluna ou diagonal do grid com o mesmo símbolo")
  
- escolha = input("Qual é a sua escolha ").
+print ("Você precisa escolher uma posição no grid para marcar sua jogada, veja o grid:")
+print ("_ _ _")
+print ("_ _ _")
+print ("_ _ _")
+print ("escolha um número de 1 a 9 para sua jogada, conforme o grid a seaguir seguir")
+print ("1 2 3")
+print ("4 5 6")
+print ("7 8 9")
+
+escolha = input("Qual é a sua escolha")
+
+grid = ["_"] * 9
+
+for indice in range(len(grid)):
+    print (grid[indice])
+    
